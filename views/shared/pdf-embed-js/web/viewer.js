06fec6f8f69deb531b51e81732c75260abeca2ca
John Flatness <john@zerocrates.org>
2019-08-28 15:14:32 -0400

    Allow cross-loading on all origins

--- views/shared/pdf-embed-js/web/viewer.js
+++ views/shared/pdf-embed-js/web/viewer.js
@@ -1546,7 +1546,7 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = ['null', window.location.origin];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
